47de3e04f20834099edf661e041419780fd651cd
Andrew R Gross <andrew.gross@cshs.org>
2017-06-05 17:21:51 -0700

    Added updated Spearman calculation code
    added improved plotting code, removed junk

--- analysis scripts/QICFIT_test_outside_data.R
+++ analysis scripts/QICFIT_test_outside_data.R
@@ -263,197 +263,104 @@ write.csv(reference.data.df.converted.full, new.file.name)
 test <- read.csv(new.file.name, row.names = 1)
 
 
-
-
-
 ########################################################################
 ########################################################################
+### Open selected dataset
 
-### Fullwood 2015, GSE69360
-df.fullwood <- read.table("Z:/Data/Andrew/reference_data/geo/GSE69360/GSE69360_RNAseq.counts.txt", sep = '\t', header = TRUE, row.names = 1)
+setwd('z:/Data/Andrew/reference_data/qicfit_ready/')                    # Sets directory
 
-### Housekeeping genes
-housekeeping.genes <- as.character(read.csv('Z:/Data/Andrew/QICFIT/housekeeping.ids_3559.csv')[,1])
+### Import dataset and sample metadata
+samples.metadata <- read.csv('METADATA_samples.csv')                    # Import sample metadata
+dataset.metadata <- read.csv('METADATA_datasets.csv')                   # Import dataset metadata
+print(dataset.metadata[c(1,2,4,5,6)])                                   # Print main dataset stats
 
-df.fullwood
+#data.frame(list.files())                                               # Lists available datasets
+selection.number <- 5                                                   # Specifies file of interest
+(sample.file <- as.character(dataset.metadata[,4][selection.number]))   # Defines file of interest
+samples.df <- read.csv(sample.file, row.names = 1)                       # Imports file of interest as current sample.df
 
-### 
-df.yu <- read.csv('Z:/Data/Andrew/reference_data/geo/yu_GSE9440.csv', row.names = 1)
-
-
-### 
-df.han <- read.csv('Z:/Data/Andrew/reference_data/geo/han_GSE35108.csv', row.names = 1)
-
-### Normalized
-TPMdata <- read.csv("z://Data/RNAseq HT neurons and tissue/Andrews_files/20160317_Sareen_rerun-29299281.tpm.csv", row.names=1)
-
-
-########################################################################
-### Format
-
-df.fullwood <- df.fullwood[6:24]
-df.fullwood <- convertIDs(df.fullwood)
-
-
-### Replace row and column names
-sampleNames <- c("iHT_03iCTR","iHT_90iOBS","iHT_77iOBS","iHT_02iOBS","iMN_87iCTR","iMN_201iCTR","aHT_1662_S6","aHT_1838_S7","aHT_1843_S8","aHT_2266_S9","iHT_02iCTR_S13","aHT_2884_S11","21-Reference","iHT_87iCTR","iHT_201iCTR","iHT_25iCTR_S16","iHT_688iCTR_","iHT_80iCTR","iHT_74iOBS","iHT_03iOBS")
-names(TPMdata) <- sampleNames
-TPMdata <- convertIDs(TPMdata)
-### Remove 02iOBS and both iMN
-TPMdata[c("iHT_02iOBS","iMN_87iCTR","iMN_201iCTR","21-Reference")] <- NULL
-sampleNames <- c("iHT_03iCTR","iHT_90iOBS","iHT_77iOBS","aHT_1662","aHT_1838","aHT_1843","aHT_2266","iHT_02iCTR","aHT_2884","iHT_87iCTR","iHT_201iCTR","iHT_25iCTR","iHT_688iCTR","iHT_80iCTR","iHT_74iOBS","iHT_03iOBS")
-
-
-### Remove Housekeeping genes from references
-test <- setdiff(row.names(ref.full),housekeeping.genes)
-test2 <- ref.full[test,]
-
-ref.full.pruned <- ref.full[setdiff(row.names(ref.full), housekeeping.genes),]
-ref.loose.pruned <- ref.loose[setdiff(row.names(ref.loose), housekeeping.genes),]
-ref.neutral.pruned <- ref.neutral[setdiff(row.names(ref.neutral), housekeeping.genes),]
-ref.tight.pruned <- ref.tight[setdiff(row.names(ref.tight), housekeeping.genes),]
-ref.supertight.pruned <- ref.supertight[setdiff(row.names(ref.supertight), housekeeping.genes),]
-
-### Generate a list of filtered reference sets
-references.list <- list(ref.full, ref.full.pruned, ref.loose, ref.loose.pruned, ref.neutral, ref.neutral.pruned, ref.tight, ref.tight.pruned, ref.supertight, ref.supertight.pruned)
-names(references.list) <- c('Ref.full', 'ref.full.pruned', 'Ref.loose', 'ref.loose.pruned', 'Ref.neutral', 'ref.neutral.pruned', 'Ref. tight', 'ref.tight.pruned', 'Ref.supertight', 'ref.supertight.pruned')
+### Report dataset and sample stats
+accession <- as.character(dataset.metadata$GEO[selection.number])       # Define the accession number of current dataset
+sample.rows <- grep(accession, samples.metadata$Accession)              # Identify corresponding rows in sample metadata
+print(dataset.metadata[selection.number,][c(1,2,4,5,6,7,8,9,10,12)])    # print dataset info
+print(samples.metadata[sample.rows[1],][c(8,7,10,11,12)])               # print dataset info from samples
+current.samples.metadata <- samples.metadata[grep(accession, samples.metadata$Accession),][c(1,2,3,4)]
+print(current.samples.metadata)
 
 ########################################################################
-### Run test using Spearman
+### Filter out low or absent genes
 
-column.num <- 4
-sample.data <- TPMdata[column.num]
-sample.data <- df.fullwood[column.num]
-sample.data <- df.yu[column.num]
-sample.data <- df.han[column.num]
-reference.df <- ref.full
-sample.data <- test[column.num]
 
-spearman.results <- spearman.calc(sample.data, reference.df)
-title <- names(spearman.results)[column.num]
-#spearman.results <- spearman.results[1]
-names(spearman.results) <- 'value'
-spearman.results$ref <- row.names(spearman.results)
+### Try alternate reference sets
+reference.input <- ref.full 
+reference.input <- ref.loose 
+reference.input <- ref.neutral 
+reference.input <- ref.tight 
+reference.input <- ref.supertight 
 
-spearman.results <- spearman.results[order(spearman.results$value, decreasing = TRUE),]
-spearman.results$ref <- factor(spearman.results$ref, levels = rev(spearman.results$ref))
+### Filter samples by max row value
+gene.maxes <- apply(samples.df,1, max)
+samples.df.filtered <- samples.df[gene.maxes >= 0,]
 
-### Plot
-g <- ggplot(data = spearman.results, aes(x = ref, y = value, fill = value)) +
-  geom_bar(stat = 'identity') +
-  scale_fill_gradientn(colors = c('white','yellow','orange','red')) +
-  coord_flip() +
-  scale_y_continuous(position = 'right', limits = c(0,100)) +
-  labs(title = row.names(spearman.results)[1],
-       x = '',
-       y = title)
-g
+### Filter samples by reference
+samples.df.filtered <- samples.df.filtered[row.names(reference.input),]
 
+
+
+
+### Filter reference input
+reference.input.filtered <- reference.input[row.names(samples.df.filtered),]
+nrow(reference.input.filtered)
+
+### Calc Spearman coeff for filtered data
+spearman.results <- spearman.calc.for.multiple.samples(samples.df.filtered, reference.input.filtered)
+
+### Report each sample's top 3
+for (sample.selection in 1:12) {print(spearman.results[sample.selection][order(spearman.results[sample.selection], decreasing = TRUE),, drop = FALSE][1:3,, drop = FALSE])}
 
 ########################################################################
-### Loop through all five reference cutoffs
+### Generate Spearman profile for each sample
 
-column.num <- 11
-### Initialize empty results lists
-spearman.results.list <- list()
-### Loop through reference sets
-for(ref.set.num in 1:length(references.list)) {                # Loop through all five reference filter levels
-  reference.input <- references.list[[ref.set.num]]            # Specify current reference list and its name
-  (reference.set.name <- names(references.list)[ref.set.num])  # Declare the name of the rererence filter set
-  spearman.results <- spearman.calc(df.fullwood[column.num], reference.input)
-  title <- names(spearman.results)[column.num]
-  names(spearman.results) <- 'value'
-  spearman.results$ref <- row.names(spearman.results)
-  ### Define order
-  spearman.results <- spearman.results[order(spearman.results$value, decreasing = TRUE),]
-  spearman.results$ref <- factor(spearman.results$ref, levels = rev(spearman.results$ref))
-  ### Add spearman results to list
-  spearman.results.list[[ref.set.num]] <- spearman.results     # Add current Spearman results table of 8 samples compared to all tissues to a list
-  names(spearman.results.list)[ref.set.num] <- reference.set.name # Name the new entry on the list with the tissue being assessed
-}
-### Generate multiplot
-barplot.list <- list()
-filter.levels <- names(references.list)
-level.num = 1
-for (spearman.results in spearman.results.list) {
-  filter.level = filter.levels[level.num]
-  level.num <- level.num + 1
-  new.plot  <- ggplot(data = spearman.results, aes(x = ref, y = value, fill = value)) +
-    geom_bar(stat = 'identity') +
-    scale_fill_gradientn(colors = c('white','yellow','orange','red')) +
-    coord_flip() +
-    scale_y_continuous(position = 'right') +
-    theme(axis.text.x = element_text(size = 10), axis.text.y = element_text(size = 7),axis.ticks.y = element_blank(),
-          legend.position = 'none', panel.background = element_rect(fill = 'grey97'),
-          plot.title = element_text(size = 14, face = 'bold', margin = margin(5,0,5,0), hjust = 0.5)) +
-    labs(title = row.names(spearman.results)[1],
-         x = '',
-         y = filter.level) 
-  barplot.list[[length(barplot.list)+1]] <- new.plot
-}
-print('All figures complete')
-multibarplot <- plot_grid(barplot.list[[1]], barplot.list[[2]], barplot.list[[3]], barplot.list[[4]], barplot.list[[5]], labels=c("F", "L", "N", "T", "ST"), ncol = 5, nrow = 1)
+### Calculate Spearman coefficents
+spearman.results <- spearman.calc.for.multiple.samples(samples.df, reference.input)
 
-multibarplot <- plot_grid(barplot.list[[1]], barplot.list[[2]], barplot.list[[3]], barplot.list[[4]], barplot.list[[5]], 
-                          barplot.list[[6]], barplot.list[[7]], barplot.list[[8]], barplot.list[[9]], barplot.list[[10]],
-                          ncol = 4, nrow = 2)
-multibarplot <- plot_grid(barplot.list[[9]], barplot.list[[10]], ncol = 2, nrow = 1)
+### Format results for heatmap
+(spearman.for.plot <- spearman.results[c(1,2,3,4,5,6,7,8,10,12,14,16,18,22,26,30,35,40,50),])
+spear.m <- melt(as.matrix(spearman.for.plot))
+spear.m$X1 <- factor(spear.m$X1, levels = rev(as.character(row.names(spearman.for.plot))))
+
+### Plot heatmap
+ggplot(data = spear.m, aes(x = X2, y = X1, fill = value)) +
+  geom_tile() +
+  geom_text(aes(label = round(value, 0)), size = 3) +
+  scale_fill_gradient(low = 'white', high = 'red') +
+  ggtitle(as.character(dataset.metadata[selection.number,][,1])) + 
+  theme(axis.text.x = element_text(angle = -90, hjust = 1))
 
 
-multibarplot
-names(df.fullwood)[column.num]
+### Subset data by tissue type
+(available.tissues <- levels(factor(current.samples.metadata$Tissue)))
+tissue.selection <- 3
+(selected.samples.metadata <- current.samples.metadata[current.samples.metadata$Tissue == as.character(available.tissues[tissue.selection]),])
+selected.samples <- as.character(selected.samples.metadata$Sample)
+spearman.for.plot <- spearman.results[selected.samples]
+spearman.for.plot <- spearman.for.plot[rev(order(apply(spearman.for.plot,1,mean))),]
+
+### Format results for heatmap
+(spearman.for.plot <- spearman.for.plot[c(1,2,3,4,5,6,7,8,10,12,14,16,18,22,26,30,35,40,50),])
+spear.m <- melt(as.matrix(spearman.for.plot))
+spear.m$X1 <- factor(spear.m$X1, levels = rev(as.character(row.names(spearman.for.plot))))
 
 
-### Breaking down Spearman correlation
+### View specific tissues
+sample.selection <- 11
+spearman.results[sample.selection][order(spearman.results[sample.selection], decreasing = TRUE),, drop = FALSE][1:4,, drop = FALSE]
 
-sample <- df.fullwood[1]
-reference.input <- ref.full
+### Report each sample's top 4
+for (sample.selection in 1:ncol(spearman.results)) {print(spearman.results[sample.selection][order(spearman.results[sample.selection], decreasing = TRUE),, drop = FALSE][1:4,, drop = FALSE])}
 
-### Generate & format empty results df
-spearman.results <- data.frame(rep(0,ncol(reference.input))) # Generate empty results table
-row.names(spearman.results) <- names(reference.input)        # Name empty results table
-names(spearman.results) <- names(sample)
-
-### Loop through each reference  tissue
-for(ref.tissue.num in 1:ncol(reference.input)) {   
-  #ref.tissue.num <- 1
-  ref.tissue.data <- reference.input[ref.tissue.num]         # Call the current tissue from the references
-  tissue <- names(ref.tissue.data)                           # Declare the name of the current tissue
-  
-  ### Add missing genes and assign value of zero
-  ref.tissue.data.2 <- ref.tissue.data[which(ref.tissue.data[,1]>0),,drop=FALSE] # Filter out missing values from tissue
-  genes.present.in.ref <- row.names(ref.tissue.data.2)         # Declare the genes present in the reference
-  genes.missing.in.query <- setdiff(genes.present.in.ref, row.names(sample)) # Declare genes in reference missing from sample
-  rows.to.add <- data.frame(rep(0,length(genes.missing.in.query)))  # Generate a zero data frame the size of the missing rows 
-  row.names(rows.to.add) <- genes.missing.in.query           # Name rows after missing rows
-  names(rows.to.add) <- names(sample)                        # Name column the same as the query 
-  sample.2 <- rbind(sample,rows.to.add)                        # Use rbind to make a full data frame containing exactly the genes in the reference
-  sample.3 <- sample.2[genes.present.in.ref, , drop = FALSE]     # Reorder sample to match reference
-  spearman.input <- cbind(sample.3, ref.tissue.data.2)           # Bind sample and reference
-  
-  ### Perform Spearman calculation
-  result <- rcorr(as.matrix(spearman.input), type = 'spearman')[[1]] # Perform spearman calculation
-  result <- round(result[2] * 100, 1)                        # Round result
-  spearman.results[tissue,] <- result                        # Add to results table
-}
-
-test2 <- cor(spearman.input, use="complete.obs", method = "spearman") 
+#spearman.results <- spearman.calc(sample.df[1],reference.input)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-spearman.calc <- function(sample, reference.input) {           # Calculate the spearman correlation between the sample and the references
-
-  return(spearman.results)
-}
