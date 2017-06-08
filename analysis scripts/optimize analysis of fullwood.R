a34c74957c1a9110604915a89dfd0dc6b0627ed8
Andrew R Gross <andrew.gross@cshs.org>
2017-06-07 18:02:15 -0700

    Created a script to open and organize Fullwood transcriptomes
    Next: run them and characterize results

--- analysis scripts/optimize analysis of fullwood.R
+++ analysis scripts/optimize analysis of fullwood.R
@@ -1,83 +1,17 @@
-### QICFIT test using outside data -- Andrew R Gross -- 2017-03-01
-### This script is used to load and format data from other sources, then to test various recognition methods
-### This script reads in expression tables; converts the IDs to ENSEMBL; Loops through each transcriptome and does a Spearman check against references; and outputs figures and tables reporting
-### The similarity of each query to each of the GTEx references
+### optimize analysis of fullwood -- Andrew R Gross -- 2017-06-05
+### This script is used to compare Fullwood sample transcriptomes directly to GTEx transcriptomes in order to improve comparison methods.
 
 ########################################################################
 ### Header
 
 library(Hmisc)
 library(biomaRt)
-#listMarts(host="www.ensembl.org")
-ensembl = useMart(host='www.ensembl.org',biomart='ENSEMBL_MART_ENSEMBL')
-#listDatasets(ensembl)
-ensembl = useMart(host='www.ensembl.org',biomart='ENSEMBL_MART_ENSEMBL',dataset="hsapiens_gene_ensembl")
-filters = listFilters(ensembl)
-attributes = listAttributes(ensembl)
-filters[grep('uniprot', filters[,1]),]
-attributes[grep('pattern', attributes[,1])]
+library(ggplot2)
+library(reshape2)
 
 ########################################################################
 ### Functions
-convertIDs <- function(dataframe) {
-  ensemblIDs <- c()
-  for (rowName in row.names(dataframe)) {
-    ensemblID <- strsplit(rowName,"\\.")[[1]][1]
-    ensemblIDs <- c(ensemblIDs,ensemblID)
-  }
-  row.names(dataframe)<-ensemblIDs
-  return(dataframe)
-}
 
-uniprot_gn.to.gene.name <- function(dataframe) {
-  ### Generate initial conversion table
-  conversion.df <- getBM(attributes=c('uniprot_gn','external_gene_name'), filters='uniprot_gn', values= row.names(dataframe), mart=ensembl)
-  unique.ids <- unique(conversion.df$uniprot_gn)
-  ### Identify the redundancy of each ID
-  redundancy.df <- data.frame(unique.ids, 'no.of.results' = rep('',length(unique.ids)), stringsAsFactors = FALSE)
-  for (row.num in 1:nrow(redundancy.df)) {
-    id = redundancy.df[row.num,][,1]
-    hits <- grep(id,conversion.df[,1])
-    redundancy.df[row.num,][2] <- length(hits)
-  }
-  ### Generate a df containing single-result IDs and multiple-result IDs
-  single.result.df <- data.frame('uniprot_gn' = c(), 'external_gene_name' = c(), stringsAsFactors = FALSE)
-  multiple.result.df <- data.frame('uniprot_gn' = c(), 'external_gene_name' = c(), stringsAsFactors = FALSE)
-  
-  for (row.num in 1:nrow(redundancy.df)) {
-    if (redundancy.df$no.of.results[row.num] == 1) {
-      row.to.add <- conversion.df[grep(redundancy.df$unique.ids[row.num], conversion.df$uniprot_gn),]
-      single.result.df <- rbind(single.result.df,row.to.add)
-    }
-    if (redundancy.df$no.of.results[row.num] > 1) {
-      all.gene.names <- conversion.df$external_gene_name[grep(redundancy.df$unique.ids[row.num], conversion.df$uniprot_gn)]
-      all.gene.names <- paste(all.gene.names, collapse = '-')
-      row.to.add <- data.frame('uniprot_gn' = redundancy.df$unique.ids[row.num], 'external_gene_name' = all.gene.names)
-      multiple.result.df <- rbind(multiple.result.df,row.to.add)    
-    }
-  }
-  ### Generate a df containing missing IDs
-  missing.ids <- setdiff(row.names(dataframe), conversion.df$uniprot_gn)
-  missing.result.df <- data.frame('uniprot_gn' = missing.ids, 'external_gene_name' = missing.ids)
-  ### Join dfs and reorder
-  conversion.df <- rbind(single.result.df, multiple.result.df, missing.result.df)
-  conversion.df <- conversion.df[match(row.names(dataframe),conversion.df$uniprot_gn),]
-  ### Convert old IDs to new IDs
-  converted.df <- dataframe
-  row.names(converted.df) <- conversion.df$external_gene_name
-  return(converted.df)
-}
-
-drop.decimals <- function(input.dataframe) {
-  input.id <- input.dataframe[,1]
-  temp.id <- strsplit(input.id, '.', fixed = TRUE)
-  output.id <- c()
-  for (id in temp.id) {output.id <- c(output.id, id[1])}
-  row.names(input.dataframe) <- output.id
-  input.dataframe <- input.dataframe[2:ncol(input.dataframe)]
-  return(input.dataframe)
-}
- 
 spearman.calc <- function(sample, reference.input) {           # Calculate the spearman correlation between the sample and the references
   spearman.results <- data.frame(rep(0,ncol(reference.input))) # Generate empty results table
   row.names(spearman.results) <- names(reference.input)        # Name empty results table
@@ -163,104 +97,68 @@ multiplot.spearman.results.list <- function(spearman.results.list) {
   return(multiboxplot)
 }
 ########################################################################
-### Import formatted data sets, references
+### Import Fullwood data and GTEx data
 
-### Set working directory
-setwd("Z:/Data/Andrew/reference_data/qicfit_ready/")
+### Import dataset and sample metadata
+samples.metadata <- read.csv('Z:/Data/Andrew/reference_data/qicfit_ready/METADATA_samples.csv')                    # Import sample metadata
+dataset.metadata <- read.csv('Z:/Data/Andrew/reference_data/qicfit_ready/METADATA_datasets.csv')                   # Import dataset metadata
+samples.metadata <- samples.metadata[grep('Fullwood', samples.metadata$Author),]
+(available.tissues <- levels(factor(samples.metadata$Tissue)))
+print(dataset.metadata[c(1,2,4,5,6)])                                   # Print main dataset stats
 
-### Acquire list of available files
-metadata.df <- read.csv("METADATA.csv", stringsAsFactors = FALSE)
-(available.transcriptomes <- list.files("files with their original ids/"))
+### Import Fullwood data
+fullwood.df <- read.csv('Z:/Data/Andrew/reference_data/qicfit_ready/GSE69360-fullwood_ID_CORR.csv', row.names = 1)
+fw.colon.df <- fullwood.df[grep('colon', samples.metadata$Tissue)]
+fw.heart.df <- fullwood.df[grep('heart', samples.metadata$Tissue)]
+fw.kidney.df <- fullwood.df[grep('kidney', samples.metadata$Tissue)]
+fw.liver.df <- fullwood.df[grep('liver', samples.metadata$Tissue)]
+fw.lung.df <- fullwood.df[grep('lung', samples.metadata$Tissue)]
+fw.stomach.df <- fullwood.df[grep('stomach', samples.metadata$Tissue)]
 
-### SELECT INPUT FILE
-selection.number <- 12
-(input.file <- available.transcriptomes[selection.number])
-#metadata.df[metadata.df$Author == "Uosaki",]
 
-### Import from available file list
-reference.data.df <- read.csv(paste0('files with their original ids/',input.file), stringsAsFactors = FALSE)
-
-### Import full references  -- The average expression of each tissue without filtering
+### Import references  -- The average expression of each tissue without filtering
 references <- read.csv('Z:/Data/Andrew/reference_data/gtex/sd.filtered.tables/gtex.full.csv',header=TRUE,row.names=1)
 
-########################################################################
-### Convert IDs to ensembl
+### Import individual reference transcriptomes for testing (sets of 8 from GTEx)
+individual.transcriptomes.df <- read.csv('Z:/Data/Andrew/reference_data/gtex/individual-ref-transcriptomes-for-testing.csv', row.names = 1)
 
-### Drop Decimal on ENSEMBL IDs
-#reference.data.df.converted.full <- drop.decimals(reference.data.df)
-
-### Identify correct input filter
-names(reference.data.df)[1]
-head(reference.data.df[1:4])
-#filters[grep('u133', filters[,1]),]
-#filters[grep('hugene', filters[,1]),]
-length(filters[grep('gene', filters[,1]),])
-filters[grep('illumina', filters[,1]),]
-#attributes[grep('ucsc', attributes[,1]),]
-
-### Declare the input, output IDs, and IDs to convert
-input.id <- 'affy_hg_u133_plus_2'
-#input.id <- 'affy_hugene_1_0_st_v1'
-#input.id <- 'with_ucsc'
-input.id <- 'wikigene_name'
-#input.id <- 'entrezgene'
-output.id <- 'ensembl_gene_id'
-current.ids <- reference.data.df[,1][1:30]
-
-### Generate initial conversion table
-conversion.df <- getBM(attributes=c(input.id, output.id), filters=input.id, values= current.ids, mart=ensembl) # ~60 s to run
-unique.ids <- unique(conversion.df[,1])
-
-### Report stats on initial conversion table
-head(unique.ids,30)
-print(paste(round(length(unique.ids)/length(current.ids),2)*100, 'percent of IDs recognized'))
-
-### Match data to convesion table and join
-reference.data.df.reordered <- reference.data.df[match(conversion.df[,1],reference.data.df[,1]),]
-reference.data.df.reordered.appended <- cbind(conversion.df,reference.data.df.reordered)
-
-### Define list of ENsEMBL IDs and empty data frame to fill
-#reference.data.df.reordered.appended <- reference.data.df.reordered.appended[1:10,]
-ensembl.col <- reference.data.df.reordered.appended[,2]
-unique.ensembl.ids <- unique(reference.data.df.reordered.appended[,2])
-single.matches.df <- reference.data.df.reordered.appended[1,]
-
-### Copy a unique row for each ENSEMBL ID to a new data frame ### ~5 minutes
-for (id in unique.ensembl.ids) {
-  matches <- grep(id, ensembl.col)
-  temp.df <- reference.data.df.reordered.appended[matches,]
-  new.row <- apply(temp.df, 2, max)
-  single.matches.df <- rbind(single.matches.df, new.row)
-}
-
-### Rename rows based on ENSEMBL IDs; remove defunct rows/columns
-single.matches.df <- single.matches.df[2:nrow(single.matches.df),]
-row.names(single.matches.df) <- single.matches.df[,2]
-reference.data.df.converted <- single.matches.df[4:ncol(single.matches.df)]
-
-### Generate a list of missing ENSEMBL IDs
-missing.ids <- setdiff(row.names(references),row.names(reference.data.df.converted))
-absent.data.df <- data.frame(matrix(0,length(missing.ids), ncol(reference.data.df.converted)) )
-names(absent.data.df) <- names(reference.data.df.converted)
-row.names(absent.data.df) <- missing.ids
-
-### Join empty rows to query data with converted ENSEMBL IDs
-reference.data.df.converted.full <- rbind(reference.data.df.converted, absent.data.df)
-reference.data.df.converted.full <- reference.data.df.converted.full[sort(row.names(reference.data.df.converted.full)),]
-
-
-head(reference.data.df.converted.full,25)
+### Import tissue collections from GTEx
+gtex.colon.df <- individual.transcriptomes.df[186:201]
+gtex.heart.df <- individual.transcriptomes.df[226:241]
+gtex.kidney.df <- individual.transcriptomes.df[242:249]
+gtex.liver.df <- individual.transcriptomes.df[250:257]
+gtex.lung.df <- individual.transcriptomes.df[258:265]
+gtex.stomach.df <- individual.transcriptomes.df[354:361]
 
 ########################################################################
-### Write file with correct IDs
+### Generate df lists to select from
 
-getwd()
+fw.samples.list <- list(fw.colon.df, fw.heart.df, fw.kidney.df, fw.liver.df, fw.lung.df, fw.stomach.df)
 
-(new.file.name <- paste0(substr(available.transcriptomes[selection.number], 0, nchar(available.transcriptomes[selection.number])-4), '_ID_CORR.csv'))
+########################################################################
+### Approach 1: Simple Spearman analysis
+### Generate Spearman profile for each sample
 
-write.csv(reference.data.df.converted.full, new.file.name)
+### Select Fullwood tissue to analyze
+samples.df <- fw.samples.list[[1]]
+print(names(samples.df)[1])
+reference.input <- reference.df
 
-test <- read.csv(new.file.name, row.names = 1)
+### Calculate Spearman coefficents
+spearman.results <- spearman.calc.for.multiple.samples(samples.df, reference.input)
+
+### Format results for heatmap
+(spearman.for.plot <- spearman.results[c(1,2,3,4,5,6,7,8,10,12,14,16,18,22,26,30,35,40,50),])
+spear.m <- melt(as.matrix(spearman.for.plot))
+spear.m$X1 <- factor(spear.m$Var1, levels = rev(as.character(row.names(spearman.for.plot))))
+
+### Plot heatmap
+ggplot(data = spear.m, aes(x = Var2, y = Var1, fill = value)) +
+  geom_tile() +
+  geom_text(aes(label = round(value*100, 0)), size = 3) +
+  scale_fill_gradient(low = 'white', high = 'red') +
+  #ggtitle(as.character(dataset.metadata[selection.number,][,1])) + 
+  theme(axis.text.x = element_text(angle = -90, hjust = 1))
 
 
 
@@ -268,6 +166,9 @@ test <- read.csv(new.file.name, row.names = 1)
 
 ########################################################################
 ########################################################################
+########################################################################
+
+
 
 ### Fullwood 2015, GSE69360
 df.fullwood <- read.table("Z:/Data/Andrew/reference_data/geo/GSE69360/GSE69360_RNAseq.counts.txt", sep = '\t', header = TRUE, row.names = 1)
